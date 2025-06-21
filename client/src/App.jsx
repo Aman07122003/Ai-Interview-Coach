25c3d04e268710f3df46b4fbbe288f219182b90f
Aman Pratap Singh <120871048+Aman07122003@users.noreply.github.com>
2025-06-21 18:22:59 +0530

    client setup

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import Home from "./components/Home";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import WhisperMock from "./components/WhisperMock";
+import VideoAnalyzeMock from "./components/VideoAnalyzeMock";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/whisper" element={<WhisperMock />} />
+        <Route path="/video-analyze" element={<VideoAnalyzeMock />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App;
+
